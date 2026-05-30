598a326d9c6bfc938f0325e3edd64b0059849b55
seenam <senakumako@gmail.com>
2026-05-30 13:54:29 +0000

    feat: Commit 4 ème test AWS Lambda dans le cadre du TP2

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-print("Event: ", json.dumps(event))
-return {
-'statusCode': 200,
-'body': json.dumps('Hello from Lambda loaded from src/main.py!')
-}
+    print("Event: ", json.dumps(event))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda loaded from src/main.py!')
+    }
